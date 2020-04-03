ad6707cc80d597f2106336cb4478ef33d7a48aba
Yegor <egor.muhladi@gmal.com>
2020-04-04 01:55:11 +0300

    Updates

---
+++ precache-manifest.13cfb3bfa6652a4bfd155a6de29a538e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5cd8dce32c2a52e3c8112afd3feb72",
+    "url": "/React-deploy/index.html"
+  },
+  {
+    "revision": "4bd1429c075695a8eaaa",
+    "url": "/React-deploy/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3be34f93457dad526712",
+    "url": "/React-deploy/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "4bd1429c075695a8eaaa",
+    "url": "/React-deploy/static/js/2.afccffa0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-deploy/static/js/2.afccffa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be34f93457dad526712",
+    "url": "/React-deploy/static/js/main.a896abf4.chunk.js"
+  },
+  {
+    "revision": "7ccb0159aec7dcb32d3d",
+    "url": "/React-deploy/static/js/runtime-main.865ffae7.js"
+  }
+]);
